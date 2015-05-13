20118a4e3716c64f808d5e834775ba4623bcb81e
Rael <rael.gc@gmail.com>
2015-05-13 13:00:25 -0300

    Adding JS sources

---
+++ leftpane-src.js
@@ -0,0 +1,46 @@
+var LeftPane = {
+	addTeam: function(id, name, url, icon, active){
+		var node = document.getElementById(id);
+        if(node == null){
+			var ul = document.getElementById('teams');
+			li = document.createElement('li');
+			li.id = id;
+			li.setAttribute("onclick", "LeftPane.switchTo('"+id+"','"+url+"')")
+			li.setAttribute("title", name)
+			li.innerHTML = name[0];
+			if( icon ){
+			  li.style.backgroundImage = "url('"+ icon +"')";
+			  li.innerHTML = ""
+			}
+			ul.appendChild(li);
+			if(active) LeftPane.setActive(id);
+		}
+	},
+	click: function(i){
+		var list = document.getElementsByTagName("li");
+		for(var j=0; j < list.length; j++){
+			if(i==j){
+				list[j].click();
+				break;
+			}
+		}
+	},
+	alert: function(team){
+		document.getElementById(team).classList.add('alert');
+	},
+	stopAlert: function(team){
+		document.getElementById(team).classList.remove('alert');
+	},
+	switchTo: function(id, url){
+		leftPane.switchTo(url)
+		LeftPane.setActive(id);
+	},
+	setActive: function(id){
+		var list = document.getElementsByTagName("li");
+		for(var i=0; i < list.length; i++){
+			list[i].className = "inactive";
+		}
+		document.getElementById(id).className = "active";
+	}
+}
+
---
+++ scudcloud-src.js
@@ -0,0 +1,74 @@
+var ScudCloud = {
+	overrideNotifications: function(){
+		TS.ui.growls.no_notifications = false;
+		TS.ui.growls.checkPermission = function() { return true; }
+		TS.ui.growls.getPermissionLevel = function() { return 'granted'; }
+		TS.ui.growls.show = function(j,k,g,o,l,b,c,m){
+			desktop.sendMessage(j,g);
+		}
+		TS.ui.banner.close();
+	},
+    count: function(){
+		var total=0; 
+		$('span.unread_highlight').not('.hidden').each(function(i){ 
+			total+= new Number($(this).text().replace('+','')); }
+		);
+		desktop.count(total.toString());
+    },
+    listChannels: function(){
+		return TS.channels.getUnarchivedChannelsForUser();
+	},
+    listTeams: function(){
+		return TS.getAllTeams();
+	},
+    quicklist: function(){
+		desktop.quicklist(ScudCloud.listChannels());
+	},
+	join: function(c){
+		return TS.channels.join(c);
+	},
+	setClipboard: function(data){
+		TS.client.ui.file_pasted_sig.dispatch(data, TS.model.shift_key_pressed);
+	},
+    preferences: function(){
+		return TS.ui.prefs_dialog.start();
+	},
+    addTeam: function(){
+		document.location = TS.boot_data.signin_url;
+	},
+	getCurrentTeam: function(){
+		var list = TS.getAllTeams();
+		if(list!=null) for(var i=0;list.length;i++){
+			if(list[i].team_url==TS.boot_data.team_url) return list[i].id;
+		}
+		return "";
+	},
+    logout: function(){
+		document.location = TS.boot_data.logout_url;
+	},
+    help: function(){
+		return TS.help_dialog.start();
+	},
+    isConnected: function(){
+        return "undefined" != typeof TS && "undefined" != typeof TS.model && TS.model.ms_connected;
+    }
+}
+var boot_data = {};
+if("undefined" != typeof TS){
+	document.onpaste = function(e){desktop.pasted(false);}
+	ScudCloud.overrideNotifications();
+	TS.ms.connected_sig.add(function(){desktop.enableMenus(true);});
+	TS.ms.disconnected_sig.add(function(){desktop.enableMenus(false);});
+	ScudCloud.onDOMReady = TS.onDOMReady;
+	TS.onDOMReady = function(){ScudCloud.overrideNotifications();ScudCloud.onDOMReady();};
+	setInterval(function(){
+		ScudCloud.count();
+	// Trying to make not all channels check this at the same time
+	}, Math.random() * (2000 - 1000) + 1000);
+    boot_data.channels = ScudCloud.listChannels();
+    boot_data.teams = ScudCloud.listTeams();
+} else {
+    boot_data.channels = new Array();
+    boot_data.teams = new Array();
+}
+boot_data
